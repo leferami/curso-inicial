f5ec56e28aab5cc682284c6b994246dff140db25
Leonel <leferami@espol.edu.ec>
2023-04-12 18:01:28 -0500

    nuevos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule
